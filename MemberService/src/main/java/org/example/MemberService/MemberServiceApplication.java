2b1ebbb8371eeffc76d9ff6c205b89045c707174
Kwoojin <dnwls488@naver.com>
2024-06-16 00:17:30 +0900

    init

---
+++ MemberService/src/main/java/org/example/MemberService/MemberServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.MemberService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberServiceApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application-member");
+        SpringApplication.run(MemberServiceApplication.class, args);
+    }
+}
